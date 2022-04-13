ae30866675cfd0aa1f7c583f71431ec5e9b967af
Miguel Larraz <miguellarraz@Miguels-MacBook-Pro-2.local>
2022-04-13 11:14:41 +0100

    comment added

--- app.R
+++ app.R
@@ -1,16 +1,18 @@
-
+#Libraries
 library(shiny)
 library('Biostrings')
 library('dplyr')
 library('ggplot2')
 library('seqTools')
-# Define UI for application that draws a histogram
+# ui
 ui <- fluidPage(
+    #title of tab
     titlePanel('Sequence Statistics'),
 
      
     sidebarLayout(
         sidebarPanel(
+            #4 options, file input box, a seq id menu, a kmer size slider bar and a translation type menu
             fileInput("file", "Please upload a .fasta file",
                       accept = ".fasta"),
             selectInput("seq", "Sequence ID",choices='Please upload a file', FALSE),
@@ -25,7 +27,7 @@ ui <- fluidPage(
 
         
         mainPanel(
-           
+           #All the outputs: Number of sequences, seq length, GCcontent, kmer, top kmers table and the base content plot
            textOutput('seq_no'),
            textOutput('Txt'),
            textOutput('GC_content'),
@@ -37,9 +39,9 @@ ui <- fluidPage(
     )
 )
 
-
+#Server with session to make it reactive
 server <- function(input, output, session) {
-    
+    #Returns NULL, so that no errors are returned before a file is uploaded
     inFile <- reactive({
         if (is.null(input$file)) {
             return(NULL)
@@ -47,12 +49,12 @@ server <- function(input, output, session) {
             input$file
         }
     })
-    
+    #Read the input, as fasta file
     fasta <- reactive({
         readDNAStringSet(inFile()$datapath)
         
     })
-    
+    #get seq names, so that menu can be updated
     seq_name <- reactive({
         if (is.null(inFile())) {
             return(NULL)
@@ -60,7 +62,7 @@ server <- function(input, output, session) {
             names(readDNAStringSet(inFile()$datapath))
         }
     })
-    
+    #Reactive, making it easier to update seq choice
     sequence <- reactive({
         if (is.null(inFile())) {
             return(NULL)
@@ -68,7 +70,7 @@ server <- function(input, output, session) {
             paste(fasta())
         }
     })
-    
+    #Df with the names and sequences in the file
     df <- reactive({
         if (is.null(inFile())) {
             return(NULL)
@@ -76,7 +78,7 @@ server <- function(input, output, session) {
             data.frame(seq_name(), sequence())
         }
     })
-    
+    #Update choices
     observe({
         updateSelectInput(
             session,
@@ -84,7 +86,7 @@ server <- function(input, output, session) {
             choices=seq_name())
         
     })
-
+#First output with number of rows in df which is the same as number of sequences
     output$seq_no<- renderText({
         if (is.null(inFile())) {
             return(NULL)
@@ -92,18 +94,22 @@ server <- function(input, output, session) {
             paste("Number of sequences in file:",nrow(df()))
         }
     })
-    
+    #GC content
     output$GC_content<- renderText({
         if (is.null(inFile())) {
             return(NULL)
         } else {
+        #Make a list with all the bases in the sequence    
         bases <- c(strsplit(df()$sequence[df()$seq_name==input$seq], "")[[1]])
+        #seq length
         len<-nchar(df()$sequence[df()$seq_name==input$seq])
+        #table with all the base frqiencies
         freq=data.frame(table(bases))
+        #Gc content
         paste("GC content:",round((( freq$Freq[freq$bases=='G'] + freq$Freq[freq$bases=='C'])/len),digits=3))
         }
         })
-    
+    #seq length with number of characters in sequnece
     output$Txt<- renderText({
         if (is.null(inFile())) {
             return(NULL)
@@ -112,17 +118,21 @@ server <- function(input, output, session) {
         }
         })
     
-    
+    #get all kmers
     output$Subseq<- renderText({
         if (is.null(inFile())) {
             return(NULL)
         } else {
+            #string with the bases
             bases2 <- c(df()$sequence[df()$seq_name==input$seq][[1]])
+            #chosen kmer size
             n<-input$kmer
+            #use count kmers to make all possible kmers and tabulate frequencies
+            #Useful because I can use it for top 5 kmers as well as the greatest gc content
             kmer_freq<-countDnaKmers(bases2, n, 1, nchar(bases2)-(n-1))
             kmer_freq_df<-as.data.frame(kmer_freq)
             colnames(kmer_freq_df)<-'freq'
-            
+            #loop through all the kmers that are present at least once, split them and if its gc content is greater than the top one before, update the GC counter and record that kmer as the current winner
             G_C<-0
             kmer_freq_df_nozero <- filter(kmer_freq_df, freq > 0)
             top_5<-head(kmer_freq_df_nozero %>% arrange(desc(freq)),n=5)
@@ -144,7 +154,7 @@ server <- function(input, output, session) {
         }
         
     })
-    
+    #Top 5 kmers, simmilar method as before, but output as table
     output$topKmer<- renderTable({
         if (is.null(inFile())) {
             return(NULL)
@@ -157,6 +167,7 @@ server <- function(input, output, session) {
         
         G_C<-0
         kmer_freq_df_nozero <- filter(kmer_freq_df, freq > 0)
+        #arange in descending order
         top_5<-head(kmer_freq_df_nozero %>% arrange(desc(freq)),n=5)
         top_5$kmer<-rownames(top_5)
         print('Top 5 k-mers:')
@@ -164,6 +175,7 @@ server <- function(input, output, session) {
         }
     }, caption="Most frequent k-mers in the sequence")
     
+    #list with all possible transaltions of the shosen sequence, and depending on user input, one is selected
     output$Translation<- renderText({
         if (is.null(inFile())) {
             return(NULL)
@@ -196,13 +208,14 @@ server <- function(input, output, session) {
             frame<-3
             direction<-2
         }
+        #Function converting the resulting values into a df, easier to extract values from
         aa2df <- function(aa) data.frame(width=width(aa), seq=as.character(aa), names=names(aa))
         current_frame <- aa2df(translations[[frame]])
         current_translation <- current_frame$seq[current_frame$names==input$seq][direction]
         paste("Translation:",current_translation,collapse = "\n")
         }
     })
-    
+    #Base content plot
     output$distPlot <- renderPlot({
         if (is.null(inFile())) {
             return(NULL)
@@ -211,17 +224,20 @@ server <- function(input, output, session) {
         len<-nchar(df()$sequence[df()$seq_name==input$seq])
         freq=data.frame(table(bases))
         freq$Proportion=freq$Freq/len
+        #arrange in alphabetical order so that bases tend to be plotted with the same colour (unless there are fuzzy bases in the data)
         freq %>% arrange(bases)
+        #More than 4 colours to account for fuzzy bases
         colours<-c('blue','black','red','green','yellow','orange')
         ggplot(freq, aes(x=bases, y=Proportion,fill = bases)) +  
-            geom_bar(stat='identity' ) +
+            geom_bar(stat='identity',color="white" , width=0.75,size=2,alpha=0.85) +
             scale_fill_manual(values = colours ) +
+            geom_text(aes(label=round(Proportion,digits=3)), vjust=1.6, color="white", size=7)+
             theme(legend.position="none") +
-            theme(text = element_text(size = 20))
+            theme(text = element_text(size = 20)) 
         
     }
         }, height = 600, width = 500)
 }
 
-# Run the application 
+# Run the app 
 shinyApp(ui = ui, server = server)
